d4f027b150a2b98da8b0790d8f78e94df396b67d
Zack Scholl <zack.scholl@gmail.com>
2015-10-02 20:24:25 -0400

    Edited

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import json
 import time
 from flask import Flask, render_template, request, jsonify
-
+import markdown2
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def hello():
+	a = {}
+	return render_template('index.html',data=a)
+
+
+@app.route("/search", methods=['POST'])
+def search():
 	a = json.load(open('test.json','r'))
 	for key in a:
 		a[key]['created_utc'] = time.strftime("%a, %d %b %Y", time.localtime(int(a[key]['created_utc'])))
-	# NEED TO CONVERT BODY TEXT FROM MARKDOWN TO HTML
+		a[key]['selftext'] = markdown2.markdown(a[key]['selftext'].replace("''","'"))
+		for i in range(len(a[key]['comments'])):
+			a[key]['comments'][i]['body'] = markdown2.markdown(a[key]['comments'][i]['body'].replace("''","'"))
 	return render_template('index.html',data=a)
 
 
